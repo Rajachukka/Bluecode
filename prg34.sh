ba36ca2a6efa64159b3920856bac5b53e7c017cd
Rajachukka <131979078+Rajachukka@users.noreply.github.com>
2023-04-28 15:54:55 +0530

    Add files via upload

---
+++ prg34.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Demonstrate array variables
+declare -a a #this is how declare array variable.
+read -p "Enter first value:" a[0]
+read -p "Enter second value:" a[1]
+read -p "Enter third value:" a[2]
+read -p "Enter fourth value:" a[3]
+echo "you entered ${a[@]}"
+echo  "the first value is : ${a[0]}"
+echo  "size of the array is ${#a[@]}"
+#End
+hhjjjkjkjk
+ssss
