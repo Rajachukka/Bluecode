18925580ae567da48058b84b05c23aae801d1ade
Rajachukka <131979078+Rajachukka@users.noreply.github.com>
2023-04-28 16:07:37 +0530

    Add files via upload

---
+++ prg45.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Line added by the trainer
+#Script to perform arithmetic operations
+echo "Enter var1 and var2"
+read v1;read v2
+opt=""
+select opt in + - \* e
+do
+#echo "you selected $opt"
+case $opt in
++) echo `echo $v1 + $v2|bc` ;;
+-) echo `echo $v1 - $v2|bc` ;;
+\*) echo `echo $v1 \* $v2|bc` ;;
+e) break ;;
+*) echo "$opt is invalid operator.."
+esac
+done
+
+#End of the program
+
+
+
+
